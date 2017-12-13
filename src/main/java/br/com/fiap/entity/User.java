2145619f5e4ac7f693749c84ea1e606cac45eb91
Amanda <pce.amanda@gmail.com>
2017-12-13 01:47:05 -0200

    corrigindo configuracoes thymeleaf

--- src/main/java/br/com/fiap/entity/User.java
+++ src/main/java/br/com/fiap/entity/User.java
@@ -24,7 +24,6 @@ public class User {
 
     @Column(name = "PASSWORD")
     @NotEmpty(message = "*Please provide your password")
-    @Transient
     private String password;
 
     @Column(name = "NAME")
